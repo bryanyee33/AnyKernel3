daf5d6aeab7ca2b55b57b5771a49e553d638b851
Pzqqt <821026875@qq.com>
2025-06-27 22:06:13 +0800

    Some text adjustments

--- _restore_anykernel.sh
+++ _restore_anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Restore kernel, vendor_boot, and vendor_dlkm image
+kernel.string=Restore kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
--- _rollback_anykernel.sh
+++ _rollback_anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Rollback kernel and vendor_dlkm image
+kernel.string=Rollback kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
